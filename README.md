c7a93768165ddf3228c63be67ed8f4c63c13aa4c
envy124 <pkgsail.dev@gmail.com>
2020-12-02 15:48:40 +0300

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# Imap-regex-finder
+
+For example, if you need to extract steam confrimation codes from your mailbox:
+
+```js
+
+const { imapFindMatchedPatterns } = require('imap-regex-finder');
+
+const creds = {
+    user: 'YOUR_EMAIL',
+    password: 'YOUR_PASSWORD',
+    host: 'imap.mail.ru',
+    port: 993,
+    tls: true,
+}
+
+const codeFindRegex = new RegExp('\\r\\n[\\t+]{5,}([A-Z0-9]{5})[\\t]{5,}<\/td>', 'gm');
+
+function recent5Hours() {
+    const delay = 5 * 3600 * 1000;
+    const recent = new Date();
+    recent.setTime(Date.now() - delay);
+    return recent;
+}
+
+async function main() {
+    const codes = await imapFindMatchedPatterns(creds, 'Steam Account Recovery', codeFindRegex);
+    console.debug(codes);
+}
+
+main()
+    .then(() => console.debug('done'))
+    .catch(err => console.error(err));
+```
---
+++ src/lib.js
@@ -0,0 +1,37 @@
+function lastFiveHours() {
+    const delay = 5 * 3600 * 1000;
+    const since = new Date();
+    since.setTime(Date.now() - delay);
+    return since;
+}
+
+async function imapFindMatchedPatterns(imapCredentials, subjectRegExp, patternRegExp, sinceDate = lastFiveHours(), debugLogger = console.debug) {
+    const connection = await imap.connect({ imap: imapCredentials });
+    debugLogger('connected to imap');
+    try {
+        await connection.openBox('INBOX');
+        const searchCriteria = ['UNSEEN', ['SINCE', sinceDate.toISOString()]];
+        const fetchOptions = {
+            bodies: ['HEADER.FIELDS (FROM SUBJECT)', 'TEXT'],
+            markSeen: true,
+        };
+        const messages = await connection.search(searchCriteria, fetchOptions);
+        debugLogger('loaded messages');
+        const patterns = messages
+            .filter(_ => subjectRegExp.test(_.parts[0].body.subject[0]))
+            .map(_ => patternRegExp.exec(_.parts[1].body))
+            .filter(Boolean)
+            .map(_ => _[1]);
+        debugLogger('found patterns');
+        return patterns;
+    }
+    catch(err) {
+        throw err;
+    }
+    finally {
+        connection.end();
+        debugLogger('imap connection closed');
+    }
+}
+
+module.exports.imapFindMatchedPatterns = imapFindMatchedPatterns;
